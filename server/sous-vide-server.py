8664e304008ca43c384268ceac6099abeb4e4ccf
paulbuxton.mail <paulbuxton.mail@gmail.com>
2012-01-31 14:23:14 +0000

    Periodic timer is basically working

--- server/sous-vide-server.py
+++ server/sous-vide-server.py
@@ -1,18 +1,16 @@
 import tornado.ioloop
 import tornado.web
-from threading import Timer
-
-
+import threading
 
 class MainHandler(tornado.web.RequestHandler):
 
 	def get(self):
-		self.render("template1.html", title="My title", temp=str(probe.readtemp()),target=str(control.gettarget()),power=str(control.Relay()))
+		self.render("template1.html", title="Sous Vide", temp=str(probe.readtemp()),target=str(control.gettarget()),power=str(control.Relay()))
 
 	def post(self):
 		control.settarget(int(self.get_argument("message")))
-#		control.UpdateControl()
-		self.render("template1.html", title="My title", temp=str(probe.readtemp()),target=str(control.gettarget()),power=str(control.Relay()))
+		control.UpdateControl()
+		self.render("template1.html", title="Sous Vide", temp=str(probe.readtemp()),target=str(control.gettarget()),power=str(control.Relay()))
 
 application = tornado.web.Application([
     (r"/", MainHandler),
@@ -33,37 +31,47 @@ class controller():
 	targettemp=5
 	controling=True
 	state=False
+
 	def gettarget(self):
 		return self.targettemp
+
 	def settarget(self,newtarget):
 		if(newtarget!=self.targettemp):
 			self.targettemp=newtarget
+
 	def enable(self):
 		self.controling=True
-		self.t=Timer(20,self.UpdateControl)
-		self.t.start()
 	def disable(self):
 		self.controling=False
 	def UpdateControl(self):
 		print 'Timer update'
 		temp=probe.readtemp()
-		if(temp>self.targettemp):
+		if(temp<self.targettemp):
 			self.state=True
 		else:
 			self.state=False
-		if(self.controling):
-			self.t.start()
+
 	def Relay(self):
 		return self.state
 
+
+
+global t
 probe=thermometer()
 control=controller()
+
+def hello():
+	control.UpdateControl()
+	t=threading.Timer(5.0,hello)
+	t.start()
+
 
 
 
 if __name__ == "__main__":
 	application.listen(8888)
-	control.enable()
+	t=threading.Timer(5.0,hello)
+	t.start()
 	tornado.ioloop.IOLoop.instance().start()
 
 
